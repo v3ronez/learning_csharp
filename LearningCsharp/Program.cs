cb849af59de480d183619503c93fe701521043d1
Henrique Veronez <v3ronez.dev@gmail.com>
2024-01-23 19:20:41 -0300

    sla to doente
    Signed-off-by: Henrique Veronez <v3ronez.dev@gmail.com>

--- LearningCsharp/Program.cs
+++ LearningCsharp/Program.cs
@@ -5,20 +5,5 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // var produto = new Product("Celular", 2000.0);
-        //
-        // produto.AddProduct(2);
-        // produto.AddProduct(2);
-        // produto.SetQuantidy(12);
-        // produto.Info();
-
-        Console.WriteLine("Welcome! Your accountNumber:");
-        string accountNumber = Console.ReadLine();
-        Console.WriteLine("Your name:");
-        string accountName = Console.ReadLine();
-        Console.WriteLine("Init deposit? ");
-        bool initDeposit = bool.Parse(Console.ReadLine());
-
-        Bank bank = new Bank(accountNumber, accountName);
     }
 }
